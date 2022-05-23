b2e200fda1827539bf1416b3644acfe908d3ff90
fckurethn <fckurethn@gmail.com>
2022-05-23 16:17:16 +0300

    errors corrections

--- terraform/modules/init-build/build.sh
+++ terraform/modules/init-build/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 aws ecr get-login-password --region "$region" | docker login --username AWS --password-stdin "$registry_id".dkr.ecr."$region".amazonaws.com
 git clone "$github_repo"
-cd app
+cd  ./demo_4/app
 docker build -t "$ecr_url":"$app_tag" .
 docker push "$ecr_url":"$app_tag"
